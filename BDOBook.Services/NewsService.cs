63efa21b8e247286f1fdcfeea45b5382356763ad
Joshua <jsjf93@hotmail.co.uk>
2023-08-16 07:13:09 +0100

    Formatting tweaks

--- BDOBook.Services/NewsService.cs
+++ BDOBook.Services/NewsService.cs
@@ -9,14 +9,14 @@ using System.Threading.Tasks;
 
 namespace BDOBook.Services
 {
-	public class NewsService : INewsService
+    public class NewsService : INewsService
     {
         private const string HttpClientName = nameof(NewsService);
 
         private readonly string _apiToken;
         private readonly IHttpClientFactory _httpClientFactory;
 
-		public NewsService(string apiToken, IHttpClientFactory httpClientFactory)
+        public NewsService(string apiToken, IHttpClientFactory httpClientFactory)
         {
             if (string.IsNullOrEmpty(apiToken))
             {
@@ -25,7 +25,7 @@ namespace BDOBook.Services
 
             _apiToken = apiToken;
             _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
-		}
+        }
 
         public async Task<GetMostRecentResponse> GetMostRecent()
         {
@@ -36,14 +36,13 @@ namespace BDOBook.Services
                 var response = await client.GetFromJsonAsync<GetMostRecentResponse>(
                     $"top?api_token={_apiToken}&categories=tech&locale=gb&limit=3",
                     new JsonSerializerOptions(JsonSerializerDefaults.Web));
-                
+
                 return response;
             }
             catch (Exception)
             {
                 throw;
             }
-
         }
     }
 }
